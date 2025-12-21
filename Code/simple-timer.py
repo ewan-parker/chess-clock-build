83709eb077b83c5bbc5974079917082456e4ae2e
ewan <ewanparker10@gmail.com>
2025-12-20 21:21:00 -0800

    Create simple-timer.py

---
+++ Code/simple-timer.py
@@ -0,0 +1,36 @@
+from machine import Pin, I2C
+import ssd1306
+import time
+
+# I2C OLED
+i2c = I2C(0, scl=Pin(5), sda=Pin(4))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+button = Pin(10, Pin.IN, Pin.PULL_UP)
+
+m = 5
+s = 11
+
+while True:
+    
+    oled.fill(0)
+    
+    
+    oled.text(str(m),10,5)
+    
+    if s >= 10:
+        oled.text(str(s),25,5)
+    else:
+        oled.text("0" + str(s),25,5)
+    oled.text(":", 18,5)
+        
+    
+    if s != 0:
+        s = s - 1
+    else:
+        m = m-1
+        s = 60
+        
+    oled.show()
+    
+    time.sleep(1)
